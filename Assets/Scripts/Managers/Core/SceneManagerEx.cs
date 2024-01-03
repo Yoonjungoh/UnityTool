0c8548a41f851fa55d46c35b1b16ea14a0342f20
YoonJungOh <atcnature@gmail.com>
2024-01-03 23:38:50 +0900

    add scene Manager ex

--- Assets/Scripts/Managers/Core/SceneManagerEx.cs
+++ Assets/Scripts/Managers/Core/SceneManagerEx.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -25,7 +26,31 @@ public class SceneManagerEx
     }
     public void LoadScene(string sceneName)
     {
-        UI_LoadingScene.Instance.LoadScene(sceneName);
+        Define.Scene sceneEnumValue = Define.Scene.Unknown;
+
+        if (Enum.TryParse(sceneName, out sceneEnumValue))
+        {
+            switch (sceneEnumValue)
+            {
+                case Define.Scene.Login:
+                    break;
+                case Define.Scene.Lobby:
+                    break;
+                case Define.Scene.Game:
+                    break;
+                case Define.Scene.StartMenu:
+                    break;
+                default:
+                    break;
+            }
+            UI_LoadingScene.Instance.LoadScene(sceneName);
+            Debug.Log($"Load {sceneName} Scene");
+        }
+        else
+        {
+            Debug.Log($"Dont exist {sceneName} Scene");
+        }
+        CurrentScene = sceneEnumValue;
     }
     string GetSceneName(Define.Scene type)
     {
